7107642dc3b8ec57f368b78048a9cc718b86ffb0
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-06-26 07:28:40 -0300

    Ajuste do update e inclusão do delete

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const app = express();
 
 app.use(express.json());
 
+//requisição POST
 app.post('/usuarios', async (req, res) => {
   try {
-    //const { email, name, age } = req.body;
-
     const novoUsuario = await prisma.user.create({
       data: {
         email: req.body.email,
@@ -24,7 +23,7 @@ app.post('/usuarios', async (req, res) => {
     res.status(500).json({ erro: 'Erro ao criar usuário' });
   }
 });
-
+// requisição GET
 app.get('/usuarios', async (req, res) => {
   try {
     const user = await prisma.user.findMany();
@@ -34,6 +33,42 @@ app.get('/usuarios', async (req, res) => {
     res.status(500).json({ erro: 'Erro ao buscar usuários' });
   }
 });
+
+
+//requisição PUT
+app.put('/usuarios/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email, name, age } = req.body;
+
+    /*const usuarioAtualizado = */await prisma.user.update({
+      where: {
+        id: req.params.id //requisição do parametro id
+      },
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age // se o campo age for inteiro no schema
+      }
+    })
+    res.status(201).json(req.body);
+  } catch (error) {
+    console.error('Erro ao atualizar usuário:', error);
+    res.status(500).json({ erro: 'Erro ao atualizar usuário' });
+  }
+});
+
+//requisição DELETE
+app.delete('/usuarios/:id', async (req, res) => {
+ // try {
+    await prisma.user.delete({
+      where: {
+        id: req.params.id //requisição do parametro id
+      },
+    })  
+// } 
+res.status(200).json({message:'Usuário Deletado com sucesso!'})     
+})
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
