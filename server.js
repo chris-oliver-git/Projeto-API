b18dad2b820d67fe3c074a438529bff8bf485c51
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-06-25 23:06:14 -0300

    git init

---
+++ server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post('/usuarios', async (req, res) => {
+  try {
+    const { email, name, age } = req.body;
+
+    const novoUsuario = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age// se o campo age for inteiro no schema
+      }
+    });
+
+    res.status(201).json(novoUsuario);
+  } catch (error) {
+    console.error('Erro ao criar usuário:', error);
+    res.status(500).json({ erro: 'Erro ao criar usuário' });
+  }
+});
+
+app.get('/usuarios', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).json({ erro: 'Erro ao buscar usuários' });
+  }
+});
+
+app.put('/usuarios/', async (req, res) => {
+  try {
+    const { email, name, age } = req.body;
+
+    const novoUsuario = await prisma.user.update({
+      where: {
+        id:
+      },
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age// se o campo age for inteiro no schema
+      }
+    });
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
