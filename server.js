2127903ae90b303f368cbe0f29dd776b6069a57d
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-06-25 23:31:29 -0300

    Update requisições

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.json());
 
 app.post('/usuarios', async (req, res) => {
   try {
-    const { email, name, age } = req.body;
+    //const { email, name, age } = req.body;
 
     const novoUsuario = await prisma.user.create({
       data: {
         email: req.body.email,
         name: req.body.name,
-        age: req.body.age// se o campo age for inteiro no schema
+        age: req.body.age // se o campo age for inteiro no schema
       }
     });
 
@@ -27,27 +27,13 @@ app.post('/usuarios', async (req, res) => {
 
 app.get('/usuarios', async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    const user = await prisma.user.findMany();
+    res.status(200).json(user);
   } catch (error) {
     console.error('Erro ao buscar usuários:', error);
     res.status(500).json({ erro: 'Erro ao buscar usuários' });
   }
 });
-
-app.put('/usuarios/', async (req, res) => {
-  try {
-    const { email, name, age } = req.body;
-
-    const novoUsuario = await prisma.user.update({
-      where: {
-        id:
-      },
-        email: req.body.email,
-        name: req.body.name,
-        age: req.body.age// se o campo age for inteiro no schema
-      }
-    });
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
